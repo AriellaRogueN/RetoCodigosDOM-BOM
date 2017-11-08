b716930c41f5faddb7fea43d44ee6daafe6cc210
AriellaRogueN <arrodriguezn@gmail.com>
2017-11-07 23:34:36 -0300

    version 0.0.1

---
+++ Twitter/README.md
@@ -0,0 +1,19 @@
+# Agregando Funcionalidad a Página web simil twitter.
+- - -
+
+###Reto de Código que agrega contenido dinámico a una página web, combinando HTML,CSS y JS. Para la realización de este reto se sigue el ejemplo otorgado por los profesores.
+
+####Flujo de trabajo.
+#####El flujo de trabajo se basa en la generacion de distintas versiones a medida que se van realizando avances en la funcionalidad, de acuerdo a los retos establecidos.
+
+
+
+
+
+
+
+
+- - -
+
+
+Pagina Web creada por Ariella Rodríguez Núñez.
--- Twitter/app.js
+++ Twitter/app.js
@@ -1,30 +1,30 @@
 
-function add () {
+function add() {
 
-/*seleccionar el texto ingresado por el usuario y darle valor*/
+    /*seleccionar el texto ingresado por el usuario y darle valor*/
     var comments = document.getElementById("usertext").value;
 
-/*Limpiar el area de texto*/
+    /*Limpiar el area de texto*/
     document.getElementById("usertext").value = "";
 
-/*creo variable para el contenedor de los comentarios, seleccionando su respectivo div*/
-var textcont = document.getElementById("commentContainer");
- 
-/*creo nuevo div para los comentarios*/
-var replicatedComments = document.createElement("div");
+    /*creo variable para el contenedor de los comentarios, seleccionando su respectivo div*/
+    var textcont = document.getElementById("commentContainer");
 
-/*validamos lo que debe ingresar el usuario*/
+    /*creo nuevo div para los comentarios*/
+    var replicatedComments = document.createElement("div");
 
-if (comments.length == 0 || comments == null) {
-    alert('Debes ingresar un mensaje');
-    return false;
-}
+    /*validamos lo que debe ingresar el usuario*/
 
-/* Para crear nodos de texto, generamos nuevas variables que van a corresponder
-al texto ingresado por el usuario y a la nueva etiqueta que se generara con ese
-texto, luego agregamos estas variables, a los elementos ya generados*/
+    if (comments.length == 0 || comments == null) {
+        alert('Debes ingresar un mensaje');
+        return false;
+    }
 
-    var newText = document.createTextNode(usertext);
+    /* Para crear nodos de texto, generamos nuevas variables que van a corresponder
+    al texto ingresado por el usuario y a la nueva etiqueta que se generara con ese
+    texto, luego agregamos estas variables, a los elementos ya generados*/
+
+    var newText = document.createTextNode(comments);
 
     var newElement = document.createElement("p");
 
